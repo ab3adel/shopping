a908fbe2eae83774b4fdd5200ea888f1cbaa0f7b
Mohammad Ismaeel <mohammadismael962@gmail.com>
2026-04-18 19:46:54 +0300

    inventroy bug fixed

--- apps/inventory-service/src/inventory-service.module.ts
+++ apps/inventory-service/src/inventory-service.module.ts
@@ -9,7 +9,7 @@ import { InventoryModule } from './inventory/inventory.module';
      TypeOrmModule.forRoot({
           host:process.env.POSTGRES_HOST,
           port : +(process.env.POSTGRES_PORT??'5432'),
-          name:process.env.POSTGRES_DB, 
+          database:process.env.POSTGRES_DB, 
           password:process.env.POSTGRES_PASSWORD,
           username:process.env.POSTGRES_USER,
           type:'postgres',
